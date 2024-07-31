29b0ee2c2b5b885fb390424fcddcecd9665a8c97
frajdzia <s.lidia@interia.pl>
2024-07-31 20:53:59 +0100

    qttt page updates
    Co-authored-by: SMG-717 <SMG-717@users.noreply.github.com>

--- game/qttt/qttt.js
+++ game/qttt/qttt.js
@@ -1,55 +1,7 @@
-const [gsize, bsize] = [3, 1]
-var grid
-var gridElements
-var statusMessage
-var turn = "X"
 
-function showTurn() {
-  if (turn == "X") {
-    statusMessage.innerText = "Player X's Turn"
-  }
-  else if (turn == "O") {
-    statusMessage.innerText = "Player O's Turn"
-  }
-  else {
-    statusMessage.innerText = ""
-  }
-}
+function QTTT(gridElement, status) {
+  const [gsize, bsize] = [3, 1]
 
-function checkWin() {
-  const [a, b, c, d, e, f, g, h, i] = [...gridElements].map(e => e.value)
-  if (a == b && b == c || a == d && d == g || a == e && e == i) {
-    return a
-  }
-  else if (e == c && c == g || e == b && b == h || e == f && f == d) {
-    return e
-  }
-  else if (i == h && h == g || i == f && f == c) {
-    return i
-  }
-  return undefined
-}
-
-function registerInput(index) {
-  if (!gridElements[index].value) {
-    gridElements[index].value = turn
-    turn = turn == "X" ? "O" : "X"
-    const win = checkWin()
-    if (!win) {
-      showTurn()
-    }
-    else if (win == "X") {
-      statusMessage.innerText = "Player X Wins!"
-      statusMessage.classList.add("win")
-    }
-    else if (win == "O") {
-      statusMessage.innerText = "Player O Wins!"
-      statusMessage.classList.add("win")
-    }
-  }
-}
-
-function renderGrid() {
   const items = []
   for (let i = 0; i < gsize; i += 1) {
     for (let j = 0; j < gsize; j += 1) {
@@ -63,7 +15,7 @@ function renderGrid() {
         `<input
           readonly
           class="square${top}${left}${right}${bottom}"
-          onclick="registerInput(${index})"
+          onclick="qttt.registerInput(${index})"
           maxLength="1" 
           autoComplete="off" 
         />`
@@ -71,13 +23,94 @@ function renderGrid() {
     }
     items.push(`<br/>`)
   }
-  grid.innerHTML = items.join("")
-  gridElements = grid.getElementsByClassName("square")
+  gridElement.innerHTML = items.join("")
+  const squares = gridElement.getElementsByClassName("square")
+
+  status.innerText = "Player X's Turn"
+  function showTurn(turn) {
+    if (turn == "X") {
+      status.innerText = "Player X's Turn"
+    }
+    else if (turn == "O") {
+      status.innerText = "Player O's Turn"
+    }
+    else {
+      status.innerText = ""
+    }
+  }
+
+  return {
+    turn: "X",
+    grid: Array(9).fill(""),
+    checkWin() {
+      const [a, b, c, d, e, f, g, h, i] = [...squares].map(e => e.value)
+      if (a == b && b == c || a == d && d == g || a == e && e == i) {
+        return a
+      }
+      else if (e == c && c == g || e == b && b == h || e == f && f == d) {
+        return e
+      }
+      else if (i == h && h == g || i == f && f == c) {
+        return i
+      }
+      return undefined
+    },
+    
+    checkFull() {
+      const entries = [...squares].map(e => e.value)
+      return !entries.some(e => !e)
+    },
+    
+    registerInput(index) {
+      if (!squares[index].value) {
+        const win = this.checkWin()
+        const full = this.checkFull()
+
+        squares[index].value = this.turn
+        this.turn = this.turn == "X" ? "O" : "X"
+        
+    
+        if (!win && full) {
+          status.innerText = "Draw."
+          status.classList.add("lose")
+          return
+        }
+        else if (!win) {
+          showTurn(this.turn)
+        }
+        else if (win == "X") {
+          status.innerText = "Player X Wins!"
+          status.classList.add("win")
+          return
+        }
+        else if (win == "O") {
+          status.innerText = "Player O Wins!"
+          status.classList.add("win")
+          return
+        }
+        
+        if (win || full) {
+          document.getElementById("reset-game-button").style = "visibility: visible;"
+        }
+        else {
+          document.getElementById("reset-game-button").style = "visibility: hidden;"
+        }
+      }
+    },
+    
+    resetGrid() {
+      for (const e of squares) {
+        e.value = ""
+      }
+      showTurn("X")
+      status.classList = ""
+      document.getElementById("reset-game-button").style = "visibility: hidden;"
+    },
+
+  }
 }
 
+var qttt
 window.onload = () => {
-  grid = document.getElementById("qttt");
-  statusMessage = document.getElementById("status");
-  renderGrid()
-  showTurn()
+  qttt = QTTT(document.getElementById("qttt"), document.getElementById("status"))
 }
