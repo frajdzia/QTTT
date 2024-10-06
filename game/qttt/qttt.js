595360e56b5d4c4ccfbc4f96f6b6283f14e5d0eb
frajdzia <s.lidia@interia.pl>
2024-10-06 13:52:27 +0100

    entanglement and collapse complete
    Co-authored-by: SMG-717 <SMG-717@users.noreply.github.com>

--- game/qttt/qttt.js
+++ game/qttt/qttt.js
@@ -1,5 +1,7 @@
 var game = {
   grid: new Array(9).fill([]),
+  ecycle: [],
+  entangles: [],
   state: "ready",
   player: "X",
   turn: 1,
@@ -9,33 +11,78 @@ var game = {
     for (const i in this.grid)
       this.grid[i] = []
   },
-  play(index) {
+  play(index, cvalue) {
     const value = this.grid[index]
     const token = this.player + this.turn
-    if (typeof(value) == "string") return
-    switch(this.state) {
+    if (typeof (value) == "string") return
+    switch (this.state) {
       case "ready":
         value.push(token)
         this.state = "second-move"
         return
       case "second-move":
         if (value.includes(token)) return
+
         value.push(token)
-        // TODO check if entangled
-        this.state = "ready"
-        this.player = this.player == "X" ? "O" : "X"
-        if (this.player == "X") this.turn += 1
+        const treeSearch = (start, goal, tokenLink, visited, choices) => {
+          visited = [...visited, start]
+          choices = [...choices, [tokenLink]]
+          const choiceIndex = choices.length - 1
+          for (const token of this.grid[start].filter(t => t != tokenLink)) {
+            const other = this.grid.findIndex((cell, i) => i != start && cell.includes(token))
+            if (other == goal) {
+              choices[choiceIndex][1] = token
+              return { success: true, cycle: visited, choices }
+            }
+            else if (other != -1) {
+              const s = treeSearch(other, goal, token, visited, choices)
+              if (s.success) {
+                s.choices[choiceIndex][1] = token
+                return s
+              }
+            }
+          }
+          return { success: false }
+        }
+
+        const search = treeSearch(index, index, token, [], [])
+        if (search.success) {
+          this.state = "entangled"
+          this.ecycle = search.cycle
+          this.entangles = search.choices
+        }
+        else {
+          this.state = "ready"
+          this.player = this.player == "X" ? "O" : "X"
+          if (this.player == "X") this.turn += 1
+        }
+
         return;
       case "entangled":
-        // TODO
+        if (cvalue) {
+          const open = this.grid[index].filter(v => v != cvalue)
+          this.grid[index] = cvalue.at(0)
+          while (open.length) {
+            const current = open.shift()
+            const i = this.grid.findIndex(v => v.includes(current))
+            if (i != -1) {
+              const others = this.grid[i].filter(v => v != current)
+              this.grid[i] = current.at(0)
+              open.push(...others)
+            }
+          }
+          this.state = "ready"
+          this.player = this.player == "X" ? "O" : "X"
+          if (this.player == "X") this.turn += 1
+        }
         return;
     }
   },
   winner() {
     const [a, b, c, d, e, f, g, h, i] = this.grid.map(e => {
-      if (typeof(e) == "string") return e
+      if (typeof (e) == "string") return e
     })
-    
+
     if (a && (a == b && b == c || a == d && d == g || a == e && e == i))
       return a
     else if (e && (e == c && c == g || e == b && b == h || e == f && f == d))
@@ -44,14 +91,16 @@ var game = {
       return i
   },
   full() {
-    return this.grid.some(e => typeof(e) != "string")
+    return !this.grid.some(e => typeof (e) != "string")
   },
 
 }
 
+var statusBox
+var gridElement
 function setupBoard() {
-  var gridElement = document.getElementById("qttt")
-  var statusBox = document.getElementById("status")
+  gridElement = document.getElementById("qttt")
+  statusBox = document.getElementById("status")
   gridElement.innerHTML = ""
   // TODO: Clear all html inside gridElement and make from scratch
   const gsize = 3
@@ -65,7 +114,7 @@ function setupBoard() {
       if (j % bsize == 0) div.classList.add("left");
       if (j == gsize - 1) div.classList.add("right");
       if (i == gsize - 1) div.classList.add("bottom");
-  
+
       div.onclick = () => registerInput(index)
       gridElement.appendChild(div)
     }
@@ -77,12 +126,29 @@ function drawBoard() {
   const divs = document.getElementsByClassName("square")
   for (const index in game.grid) {
     const value = game.grid[index]
-    if (typeof(value) == "string") {
+    if (typeof (value) == "string") {
       divs[index].innerText = value
       divs[index].classList.add("collapsed")
+      divs[index].classList.remove("entangled")
     }
     else {
-      divs[index].innerText = value.join(",")
+      if (game.state != "entangled") {
+        divs[index].innerText = value.join(", ")
+      }
+      else {
+        divs[index].innerHTML = ""
+        for (const choice of value) {
+          const span = document.createElement("span")
+          span.innerText = choice
+          span.onclick = () => {
+            console.log(`Chose value ${choice} for square ${index}`)
+            game.play(index, choice)
+            drawBoard()
+          }
+          divs[index].appendChild(span)
+        }
+      }
+
     }
   }
 }
@@ -96,8 +162,16 @@ function registerInput(index) {
     game.status = "end"
   }
   else if (winner) {
-    statusBox.innerText = `Winner is ${game.player}`
+    statusBox.innerText = `Winner is ${winner}`
     game.status = "end"
+  }
+  else if (game.state == "entangled") {
+    statusBox.innerText = `We're entangled! ${JSON.stringify(game.ecycle)}, ${JSON.stringify(game.entangles)}`
+
+    const divs = document.getElementsByClassName("square")
+    for (const index of game.ecycle) {
+      divs[index].classList.add("entangled")
+    }
   }
 }
 
